8afa3af2159bfa5ebcaa277632b29871f9eaf584
majeedsaida <saidatua46@gmail.com>
2024-10-27 21:50:52 +0000

    string_methods.py

---
+++ Python_Basics/string_methods.py
@@ -0,0 +1,8 @@
+# string_methods.py
+
+text = "Hello, World!"
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("Replace:", text.replace("World", "Python"))
+words = ["Python", "is", "fun"]
+print("Join:", "-".join(words)) 
