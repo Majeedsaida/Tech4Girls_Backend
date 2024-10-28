8e84b0cefcaa0ffbe81d2558404e644ded593b13
majeedsaida <saidatua46@gmail.com>
2024-10-28 00:03:05 +0000

    string_indexing.py

---
+++ Python_Basics/string_indexing.py
@@ -0,0 +1,8 @@
+# string_indexing.py
+
+sentence = "Python is powerful and easy to learn."
+
+print("Length of sentence:", len(sentence))
+print("First character:", sentence[0])
+print("Last character:", sentence[-1])
+print("Word:", sentence[10:19])
