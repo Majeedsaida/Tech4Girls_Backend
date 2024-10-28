538ff41ae0465e6ac6418ea6fe253681bf293e74
majeedsaida <saidatua46@gmail.com>
2024-10-28 02:06:32 +0000

    arithmetic_and_comparison.py

---
+++ Python_Basics/Python_Control_Flow/arithmetic_and_comparison.py
@@ -0,0 +1,22 @@
+# arithmetic_and_comparison.py
+
+x = 10
+y = 3
+
+print(f"Addition: {x + y}")
+print(f"Subtraction: {x - y}")
+print(f"Multiplication: {x * y}")
+print(f"Division: {x / y}")
+print(f"Floor Division: {x // y}")
+print(f"Exponentiation: {x ** y}")
+print(f"Modulus: {x % y}")
+
+
+
+print(f"x -eq y: {x == y}")     # Equal to
+print(f"x -ne y: {x != y}")     # Not equal to
+print(f"x -gt y: {x > y}")      # Greater than
+print(f"x -lt y: {x < y}")      # Less than
+print(f"x -ge y: {x >= y}")     # Greater than or equal to
+print(f"x -le y: {x <= y}")     # Less than or equal to
+
