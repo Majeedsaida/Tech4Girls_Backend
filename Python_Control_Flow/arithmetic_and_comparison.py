f74620d24b5d90614f2e06f09948890b54194ac7
majeedsaida <saidatua46@gmail.com>
2024-10-28 03:29:50 +0000

    arithmetic_and_comparison.py

---
+++ Python_Control_Flow/arithmetic_and_comparison.py
@@ -0,0 +1,21 @@
+# arithmetic_and_comparison.py
+
+a = 10
+b = 3
+
+print(f"Addition: {a + b}")
+print(f"Subtraction: {a - b}")
+print(f"Multiplication: {a * b}")
+print(f"Division: {a / b}")
+print(f"Floor Division: {a // b}")
+print(f"Exponentiation: {a ** b}")
+print(f"Modulus: {a % b}")
+
+
+
+print(f"x == y: {a == b}")  
+print(f"x != y: {a != b}")
+print(f"x < y: {a < b}")
+print(f"x > y: {a > b}")
+print(f"x <= y: {a <= b}")
+print(f"x >= y: {a >= b}")
